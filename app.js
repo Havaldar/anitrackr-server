1983e3de4db524dc6c7ccbddc663beadb7cd1461
Havaldar <ah2717@nyu.edu>
2016-07-21 22:55:52 -0700

    added stuff

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 var express = require('express');
 var app = express();
-
 var schedule = require('node-schedule');
-var twilio = require('twilio-api');
-var cli = new twilio.Client("AC8291e1a6d67aafe528bcdc77f25f3e4f","c6021b739324a11dbedcfe5129eae946");
+var request = require('request');
+var cheerio = require('cheerio');
 
-app.use(cli.middleware());
-app.listen(3000, function() { console.log("serve running on port 3000" ); });
+// var twilio = require('twilio-api');
+// var cli = new twilio.Client("AC8291e1a6d67aafe528bcdc77f25f3e4f","c6021b739324a11dbedcfe5129eae946");
+
+// app.use(cli.middleware());
+app.listen(3000, function() { console.log("server running on port 3000" ); });
 
 
 
-app.get('/user/:username', function(req,res) {
+app.get('/api/user/:username', function(req,res) {
 	res.json({
 		name: req.params.username
 	});
 });
+
+
+app.get('/api/anime/:link', function(req, res) {
+	var url = "http://www.watch-anime.net/" + req.params.link;
+	request(url, function (error, response, html) {
+		if (!error) {
+			var $ = cheerio.load(html), episodes = $(".wpa_pag ul li");
+			res.json({
+				number_of_episodes: episodes.length
+			});
+		}
+		else { 
+			console.log("We've encountered a problem!");
+		}
+	});
+}); 
